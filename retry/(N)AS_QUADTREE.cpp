8b1197a3d0e547f7002096829fc89e000f11c21a
ky8778 <gunyoung8778@gmail.com>
2020-01-29 08:43:14 +0900

    AS_QUADTREE.cpp fail. need to retry

---
+++ retry/(N)AS_QUADTREE.cpp
@@ -0,0 +1,18 @@
+
+//! 20200128
+// TODO Divide & Conquer
+// AS_QUADTREE : https://www.algospot.com/judge/problem/read/QUADTREE
+
+#include<cstdio>
+#include<string>
+
+using namespace std;
+
+int N;
+
+
+int main(){
+    
+    
+    return 0;
+}
