9a4606129ee67d1dbec8235f3ad05501d9d839fd
ky8778 <gunyoung8778@gmail.com>
2020-03-21 18:40:20 +0900

    BJ11729_하노이탑순서.cpp Divide&Conquer solution uploaded(need to retry)

--- retry/BJ11729_하노이탑순서.cpp
+++ retry/BJ11729_하노이탑순서.cpp
@@ -1,5 +1,5 @@
-//! 2020.03.19
 // TODO BJ11729_하노이탑이동순서
+//! 2020.03.19
 
 //! 2020.03.20
 /* 분할정복 알고리즘을 잘못 구상하였다.
@@ -86,8 +86,6 @@ int main(){
 #include<vector>
 using namespace std;
 
-
-const int INF = 987654321;
 int N,K;
 vector <pair<int,int>> result;
 vector <int> inData[3];
@@ -98,24 +96,34 @@ void move(int from,int to){
     inData[from].pop_back();
     inData[to].push_back(val);
     result.push_back(make_pair(from+1,to+1));
+    K++;
 }
 
-void getResult(int from, int to){
-    int mid = -1;
-    for(int i=0;i<3;i++){
-        if(mid != from && mid != to){
-            mid = i;
-            break;
-        }
+void getResult(int from, int to, int n){
+    if(n==1){
+        move(from,to);
+        return;
     }
-
+    else{
+        int next = -1;
+        for(int i=0;i<3;i++){
+            if(i != from && i != to){
+                next = i;
+                break;
+            }
+        }
+        getResult(from,next,n-1);
+        getResult(from,to,1);
+        getResult(next,to,n-1);
+    }    
 }
 
 int main(){
     scanf("%d",&N);
 
-    for(int i=0;i<3;i++) inData[i].push_back(INF);
     for(int i=N;i>0;i--) inData[0].push_back(i);
+
+    getResult(0,2,N);
 
     printf("%d\n",K);
     for(int i=0;i<result.size();i++){
