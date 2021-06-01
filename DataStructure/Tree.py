b791758967a8ec89e68a633cf9f5377aced92121
ky8778 <gunyoung8778@gmail.com>
2021-06-01 23:21:36 +0900

    Tree Traversal Method

--- DataStructure/Tree.py
+++ DataStructure/Tree.py
@@ -10,3 +10,19 @@ class BinarySearchTree(object):
 
     def makeRoot(self, data):
         self.root = Node(data)
+
+    def traversal(self):
+        if self.root:
+            return self.traversalNode(self.root)
+        else:
+            print("Tree is empty")
+            return []
+
+    def traversalNode(self, current_node):
+        ret = []
+        if current_node.left:
+            ret.extend(self.traversalNode(current_node.left))
+            ret.extend([current_node.data])
+        if current_node.right:
+            ret.extend(self.traversalNode(current_node.right))
+        return ret
