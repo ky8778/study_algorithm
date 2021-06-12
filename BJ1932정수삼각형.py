1a74d19962ac9c979254af1144d5c5c34130e6d8
ky8778 <gunyoung8778@gmail.com>
2021-06-12 13:41:30 +0900

    [DP] BJ1932_정수삼각형.py added

--- BJ1932정수삼각형.py
+++ BJ1932정수삼각형.py
@@ -1,10 +1,27 @@
 # DP
-# 2021.06.07
+# 2021.06.12
+
+def getResult(input_map, memo, y, x, N):
+    # DP
+    if memo[y][x] != -1:
+        return memo[y][x]
+    # bottom
+    if y >= N-1:
+        return input_map[y][x]
+
+    # DFS
+    memo[y][x] = input_map[y][x] + max(getResult(input_map, memo, y+1, x, N), getResult(input_map, memo, y+1, x+1, N))
+    
+    return memo[y][x]
 
 N = int(input())
-Triangle
+my_map = []
+memo = [[-1 for _ in range(n)] for n in range(1, N+1)] # max route from this node to bottom
 
-for 
+for n in range(N):
+    my_map.append(list(map(int, input().split())))
+
+print(getResult(my_map, memo, 0, 0, N))
 
 ''' input
 5
